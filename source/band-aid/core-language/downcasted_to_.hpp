6162ed9ec50b099c732af79f4d8361895501ccd1
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-08-28 05:55:50 +0200

    Only actual downcast for `downcasted_to_`.

--- source/band-aid/core-language/downcasted_to_.hpp
+++ source/band-aid/core-language/downcasted_to_.hpp
@@ -6,17 +6,32 @@
 
 namespace cppx
 {
-    template< class Derived, class Type,
+    namespace impl
+    {
+        template< class Derived, class Type >
+        inline auto downcasted_to_( ptr_<Type> p )
+            -> ptr_<Derived>
+        {
+            const auto pd = dynamic_cast<ptr_<Derived>>( p );
+            return static_cast<ptr_<Type>>( pd ) == p? pd : nullptr;
+        }
+    }
+
+    template<
+        class Derived,
+        class Type,
         class = enable_if_t<is_base_and_derived<Type, Derived>>
         >
     inline auto downcasted_to_( ptr_<Type> p )
         -> ptr_<Derived>
-    { return dynamic_cast<ptr_<Derived>>( p ); }
+    { return impl::downcasted_to_<Derived>( p ); }
 
-    template< class Derived, class Type,
+    template<
+        class Derived,
+        class Type,
         class = enable_if_t<is_base_and_derived<Type, Derived>>
         >
     inline auto downcasted_to_( ptr_<const Type> p )
         -> ptr_<const Derived>
-    { return dynamic_cast<ptr_<const Derived>>( p ); }
+    { return impl::downcasted_to_<const Derived>( p ); }
 }
